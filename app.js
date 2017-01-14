25f0256457fa315aa4d7d3092e8749a91f49293a
MATT BRUTON <the.bruton@gmail.com>
2017-01-13 18:35:52 -0600

    added basic free bootstrap theme to project

--- app.js
+++ app.js
@@ -6,4 +6,12 @@ const APP = EXPRESS();
 
 let port = 5000;
 
+APP.get("/", (req, res) => {
+    res.send("here's some text");
+});
+
+APP.get("/books", (req, res) => {
+    res.send("here's some books");
+});
+
 APP.listen(port, (err) => console.log(`running server on port ${port}`));
