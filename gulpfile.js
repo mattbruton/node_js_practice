d15fdafc90d43b9eeb9e6db3e646dc57c9d9303d
MATT BRUTON <the.bruton@gmail.com>
2017-01-15 17:56:33 -0600

    configured inject task in gulpfile to generate references to css/js in index.html

--- gulpfile.js
+++ gulpfile.js
@@ -17,12 +17,22 @@ gulp.task('style', () => {
 gulp.task('inject', () => {
 
     let wiredep = require('wiredep').stream;
+    let inject = require('gulp-inject');
+
+    let injectSrc = gulp.src(['./public/css/*.css',
+                              './public/js/*.js'], {read: false});
+    let injectOptions = {
+        ignorePath: '/public'
+    };
+
     let options = {
         bowerJson: require('./bower.json'),
-        directory: './public/lib'
+        directory: './public/lib',
+        ignorePath: '../../public'
     }
 
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
 });
