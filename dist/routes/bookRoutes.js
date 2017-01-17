56a2102536207980b0fc53eae36e14901f43233b
MATT BRUTON <the.bruton@gmail.com>
2017-01-17 01:35:14 -0600

    implemented route and view for single book page. added id property to book objects in order to iterate through placeholder images pulled in through link in bootstrap template

--- dist/routes/bookRoutes.js
+++ dist/routes/bookRoutes.js
@@ -13,26 +13,31 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var bookRouter = _express2.default.Router();
 
 var books = [{
+    id: 0,
     title: 'Book 1',
     genre: 'Cooking',
     author: 'Dogman McCatface',
     read: false
 }, {
+    id: 1,
     title: 'Book 2',
     genre: 'Self-Help',
     author: 'Fishman Birdface',
     read: true
 }, {
+    id: 2,
     title: 'Book 3',
     genre: 'Nonfiction',
     author: 'Birdman McDogface',
     read: false
 }, {
+    id: 3,
     title: 'Book 4',
     genre: 'Science Fiction',
     author: 'Kitty Dogford',
     read: true
 }, {
+    id: 4,
     title: 'Book 5',
     genre: 'Biography',
     author: 'Zerba Zerberson',
@@ -40,7 +45,7 @@ var books = [{
 }];
 
 bookRouter.route('/').get(function (req, res) {
-    res.render('books', {
+    res.render('bookListView', {
         title: 'Read some books',
         nav: [{
             Link: '/books',
@@ -53,8 +58,19 @@ bookRouter.route('/').get(function (req, res) {
     });
 });
 
-bookRouter.route('/single').get(function (req, res) {
-    res.send('Hello, Single Book');
+bookRouter.route('/:id').get(function (req, res) {
+    var id = req.params.id;
+    res.render('bookView', {
+        title: 'Read some books',
+        nav: [{
+            Link: '/books',
+            Text: 'Books'
+        }, {
+            Link: '/authors',
+            Text: 'Authors'
+        }],
+        book: books[id]
+    });
 });
 
 exports.default = bookRouter;
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -4,30 +4,35 @@ const bookRouter = express.Router();
 
 let books = [
     {
+        id: 0,
         title: 'Book 1',
         genre: 'Cooking',
         author: 'Dogman McCatface',
         read: false
     },
     {
+        id: 1,
         title: 'Book 2',
         genre: 'Self-Help',
         author: 'Fishman Birdface',
         read: true
     },
     {
+        id: 2,
         title: 'Book 3',
         genre: 'Nonfiction',
         author: 'Birdman McDogface',
         read: false
     },
     {
+        id: 3,
         title: 'Book 4',
         genre: 'Science Fiction',
         author: 'Kitty Dogford',
         read: true
     },
     {
+        id: 4,
         title: 'Book 5',
         genre: 'Biography',
         author: 'Zerba Zerberson',
@@ -37,7 +42,7 @@ let books = [
 
 bookRouter.route('/')
     .get((req, res) => {
-        res.render('books',
+        res.render('bookListView',
         {
             title: 'Read some books',
             nav:
@@ -53,9 +58,23 @@ bookRouter.route('/')
         });
     });
 
-bookRouter.route('/single')
+bookRouter.route('/:id')
     .get((req, res) => {
-        res.send('Hello, Single Book');
+        let id = req.params.id;
+        res.render('bookView',
+        {
+            title: 'Read some books',
+            nav:
+            [{
+                Link: '/books',
+                Text: 'Books'
+            },
+            {
+                Link: '/authors',
+                Text: 'Authors'
+            }],
+            book: books[id]
+        });
     });
 
 export default bookRouter;
